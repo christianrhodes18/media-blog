0a389c0cbc44f98b8bff5ef4ca8dd966cbb91f2a
Christian Rhodes <thechristianrhodes@gmail.com>
2023-08-24 23:23:59 -0600

    start mdx util for managing blog posts

---
+++ app/utils/mdx.ts
@@ -0,0 +1,20 @@
+import path from 'path'
+import fs from 'fs'
+import matter from 'gray-matter'
+import readingTime from 'reading-time'
+import { sync } from 'glob'
+
+const articlesPath = path.join(process.cwd(), 'data/articles')
+
+export async function getSlug() {
+  const paths = sync(`${articlesPath}/*.mdx`)
+
+  return paths.map((path) => {
+    // holds the paths to the directory of the article
+    const pathContent = path.split('/')
+    const fileName = pathContent[pathContent.length - 1]
+    const [slug, _extension] = fileName.split('.')
+
+    return slug
+  })
+}
