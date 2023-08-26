ce36a9a948494099b5119f49c65c98405e52fea9
Christian Rhodes <thechristianrhodes@gmail.com>
2023-08-25 23:52:47 -0600

    start components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,14 @@
+// Footer.tsx
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto">
+        <p>&copy; 2023 Your Company. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
