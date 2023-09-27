baf70d4bc530c6f77c5df4aa8f89e1df85199ad0
Christian Rhodes <thechristianrhodes@gmail.com>
2023-09-27 11:34:07 -0600

    improving filter logic

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,15 +1,27 @@
 //"use client"
+//import { useEffect } from 'react'
 import Link from 'next/link'
 import { getAllArticles } from "../utils/mdx"
 import ArticleArchiveBoxy from '../components/Cards/ArticleArchiveBoxy'
 import FilterButton from '../components/FilterButton'
-import { useEffect } from 'react'
+
+//TRYING OTHER SEARCHPARAMS CALL - export default async function Posts({ searchParams }: { searchParams: { tag: string, aesthetic: string, date: string }}) {
+
 
 // TODO: make async function and get tags and aesthetics from mdx files
-export default async function Posts({ searchParams }: { searchParams: { tag: string, aesthetic: string, date: string }}) {
-  const selectedTag = (searchParams.tag || "all") as string // all is default state
-  const selectedAesthetic = (searchParams.aesthetic || "all") as string
-  const selectedDate = (searchParams.date || "newest") as string
+export default async function Posts({ searchParams }: { searchParams: { [key: string]: string | string[]} }) {
+  //const selectedTag = (searchParams.tag || "all") as string // all is default state
+  //const selectedAestheticString = (searchParams.aesthetic || "all") as string
+
+  // get tags from searchParams and convert to strings
+  const selectedTagString: string = Array.isArray(searchParams.tag) ? searchParams.tag.join(',') : searchParams.tag || "all"
+  const selectedTagArray: string[] = selectedTagString.split(',')
+
+  // get aesthetics from searchParams and convert to strings
+  const selectedAestheticString: string = Array.isArray(searchParams.aesthetic) ? searchParams.aesthetic.join(',') : searchParams.aesthetic || "all"
+  const selectedAestheticArray: string[] = selectedAestheticString.split(',')
+
+  //const selectedDate = (searchParams.date || "newest") as string
 
   const articles = await getAllArticles()
   articles
@@ -20,12 +32,12 @@ export default async function Posts({ searchParams }: { searchParams: { tag: str
       return 0
     })
   const posts = articles.reverse()
-
+  
   const updatePosts = () => {
     // filter posts by tag
-    if (selectedTag !== "all") {
+    if (selectedTagString !== "all") {
       posts.filter((post: { data: { tags: string[] } }) => {
-        return post.data.tags.includes(selectedTag)
+        return post.data.tags.includes(selectedTagString)
       })
     }
   }
@@ -38,7 +50,7 @@ export default async function Posts({ searchParams }: { searchParams: { tag: str
   // update posts when selectedTag changes
   // useEffect(() => {
   //   updatePosts()
-  // }, [selectedTag, selectedAesthetic, selectedDate])
+  // }, [selectedTag, selectedAestheticString, selectedDate])
 
     return (
       <div className="w-[90%] md:w-[85%] flex flex-col max-w-5xl mx-auto sm:mt-12 mb-12">
@@ -55,6 +67,52 @@ export default async function Posts({ searchParams }: { searchParams: { tag: str
             type="date"
           /> */}
         </div>
+
+        {/* display selected filters if there are any */}
+        {selectedTagString !== "all" || selectedAestheticString !== "all" && (
+          <div className="flex flex-row gap-2 mb-4">
+
+            {/* display selected tags if any */}
+            {selectedTagString !== "all" && (
+              <div className="flex cardBGLightDark rounded-3xl px-4 h-8 my-auto">
+                <p className="font-bold mr-2 my-auto">tags: </p>
+                {/* list current aesthetic filters */}
+                <div className="flex gap-2 align-middle">
+                  {selectedTagArray.map((tag) => {
+                    return (
+                      <p key={tag} className="my-auto italic">{tag}</p>
+                    )
+                  })}
+                </div>
+                {/* clear tag filters */}
+                <button className="pl-2">
+                  x
+                </button>
+              </div>
+            )}
+
+            {/* display selected aesthetics if any */}
+            {selectedAestheticString !== "all" && (
+              <div className="flex cardBGLightDark rounded-3xl px-4 h-8 my-auto">
+                <p className="font-bold mr-2 my-auto">aesthetics: </p>
+                {/* list current aesthetic filters */}
+                <div className="flex gap-2 align-middle">
+                  {selectedAestheticArray.map((aesthetic) => {
+                    return (
+                      <p key={aesthetic} className="my-auto italic">{aesthetic}</p>
+                    )
+                  })}
+                </div>
+                {/* clear aesthetic filters */}
+                <button className="pl-2">
+                  x
+                </button>
+              </div>
+            )}
+
+          </div>
+        )}
+
         <hr className="mb-8" />
         <div className="flex flex-col mx-auto md:mx-0 gap-6">
           {posts.map((frontMatter: { slug: any; title: string; publishedAt: string; excerpt: string, tags: string[]; coverImage: string }) => {
